eb967a590225e1fbd0c5683397d4bdecc07913b0
unknown <umeryamin542@gmail.com>
2025-11-15 17:49:46 +0500

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+import { app } from './app.js';
 import connectDB from './db/db.js'
 import dotenv from 'dotenv';
 
 // Load environment variables
 dotenv.config();
 
-connectDB();
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`MongoDB Connection Failed: ${err}`);
+    })
