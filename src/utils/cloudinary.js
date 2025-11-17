2e0a4de82a9a64e0247774bf6f4d4aad9ac147be
Tehseen Javed <tehseenjaved100@gmail.com>
2025-11-17 20:15:00 +0500

    configer cloudinary and multer

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,24 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        });
+
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary: ", response.url);
+        return response;
+    } catch (error) {
+        // remove the locally saved temporary file as the upload opperation got failed
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
