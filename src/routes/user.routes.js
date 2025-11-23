8def854e4e21a51c91faf84f2b8a891b3e4c28b3
Tehseen Javed <tehseenjaved100@gmail.com>
2025-11-23 07:49:28 +0500

    rearrange routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -31,10 +31,10 @@ router.route("/register").post(
     registerUser
 );
 router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
